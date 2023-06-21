b77a418e50c142a31ef55d60a1fddc4d59879fc9
Tanrari <86423821+Tanrari@users.noreply.github.com>
2023-06-21 18:33:53 +0300

    #6

--- src/main/java/ru/web/dto/Singer.java
+++ src/main/java/ru/web/dto/Singer.java
@@ -1,22 +1,20 @@
 package ru.web.dto;
-
-import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.sql.Blob;
-import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
-@Table
+@Table(name = "singer")
 public class Singer implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     @Column(name = "ID")
     private Long id;
 
@@ -24,15 +22,16 @@ public class Singer implements Serializable {
     @Column(name = "VERSION")
     private int version;
 
-    @NotBlank(message = "{validation.firstname.NotEmpty.message}")
-    @Size(min = 3, max = 60,message = "{validation.firstname.Size.message}")
+    @NotBlank(message="{validation.firstname.NotBlank.message}")
+    @Size(min=2, max=60, message="{validation.firstname.Size.message}")
     @Column(name = "FIRST_NAME")
     private String firstName;
 
-    @NotBlank(message = "{validation.lastname.NotEmpty.message}")
-    @Size(min = 1, max = 40, message = "{validation.lastname.Size.message}")
+    @NotBlank(message="{validation.lastname.NotBlank.message}")
+    @Size(min=1, max=40, message="{validation.lastname.Size.message}")
     @Column(name = "LAST_NAME")
     private String lastName;
+
     @NotNull
     @Temporal(TemporalType.DATE)
     @Column(name = "BIRTH_DATE")
@@ -41,50 +40,50 @@ public class Singer implements Serializable {
     @Column(name = "DESCRIPTION")
     private String description;
 
-    @Basic(fetch = FetchType.LAZY)
+    @Basic(fetch= FetchType.LAZY)
     @Lob
-    //    @Type(type = "org.hibernate.type.TextType")
     @Column(name = "PHOTO")
     private byte[] photo;
 
+
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public int getVersion() {
         return version;
     }
 
-    public void setVersion(int version) {
-        this.version = version;
-    }
-
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public java.util.Date getBirthDate() {
-        return birthDate;
-    }
-
     public void setBirthDate(java.util.Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public java.util.Date getBirthDate() {
+        return birthDate;
     }
 
     public String getDescription() {
@@ -104,25 +103,20 @@ public class Singer implements Serializable {
     }
 
     @Transient
-    public String getBirthDateString(){
-        String  birthDateString = "";
-        if(birthDate !=null){
+    public String getBirthDateString() {
+        String birthDateString = "";
+        if (birthDate != null) {
+            System.out.println();
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            birthDateString  = sdf.format(birthDate);
-         }
+            birthDateString = sdf.format(birthDate);
+        }
         return birthDateString;
     }
 
     @Override
     public String toString() {
-        return "Singer{" +
-                "id=" + id +
-                ", version=" + version +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", birthDate=" + birthDate +
-                ", description='" + description + '\'' +
-                ", photo=" + photo +
-                '}';
+        return "Singer - Id: " + id + ", First name: " + firstName
+                + ", Last name: " + lastName + ", Birthday: " + birthDate
+                + ", Description: " + description;
     }
 }
--- src/main/java/ru/web/dto/SingerController.java
+++ src/main/java/ru/web/dto/SingerController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
@@ -21,9 +20,67 @@ import java.util.Locale;
 @Controller
 public class SingerController {
     private final Logger logger = Logger.getLogger(SingerController.class);
-
     private SingerService singerService;
     private MessageSource messageSource;
+
+    @Autowired
+    public void setSingerService(SingerService singerService){
+        this.singerService = singerService;
+    }
+
+    @Autowired
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+
+    @RequestMapping(value = "/{id}", params = "form", method = RequestMethod.POST)
+    public String update(@Valid Singer singer, BindingResult bindingResult, Model uiModel,
+                         HttpServletRequest httpServletRequest, RedirectAttributes redirectAttributes,
+                         Locale locale){
+        logger.info("Updating singer");
+        if (bindingResult.hasErrors()){
+//            System.out.println("DATe"+singer.getBirthDate().toString());
+            uiModel.addAttribute("message",
+                    new Message("error", messageSource.getMessage("singer_save_fail",
+                            new Object[]{}, locale)));
+            uiModel.addAttribute("singer", singer);
+            return "singers/update";
+        }
+        uiModel.asMap().clear();
+        redirectAttributes.addFlashAttribute("message", new Message("success",
+                messageSource.getMessage("singer_save_success", new Object[]{}, locale)));
+        singerService.save(singer);
+        return "redirect:/singers/" +
+                UrlUtil.encodeUrlPathSegment(singer.getId().toString(), httpServletRequest);
+    }
+
+    @RequestMapping(value = "/{id}", params = "form", method = RequestMethod.GET)
+    public String updateForm(@PathVariable("id") Long id, Model uiModel){
+        uiModel.addAttribute("singer",singerService.findById(id));
+        return "singers/update";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String create(@Valid Singer singer,BindingResult bindingResult,
+                         Model uiModel,HttpServletRequest httpServletRequest,
+                         RedirectAttributes redirectAttributes,
+                         Locale locale){
+        logger.info("Creating singer");
+        if(bindingResult.hasErrors()){
+            uiModel.addAttribute("message",new Message("error",
+                    messageSource.getMessage("singer_save_fail",new Object[]{},locale)));
+            uiModel.addAttribute("singer",singer);
+            return "singers/create";
+        }
+
+        uiModel.asMap().clear();
+        redirectAttributes.addFlashAttribute("message",new Message("success",
+                messageSource.getMessage("singer_save_success", new Object[]{},locale)));
+        logger.info("Singer id: "+singer.getId());
+        singerService.save(singer);
+        return "redirect:/singers/";
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public String list(Model model){
@@ -33,6 +90,8 @@ public class SingerController {
         logger.info("No. of singers"+singers.size());
         return "singers/list";
     }
+
+
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public String show(@PathVariable("id")Long id, Model uiModel){
         Singer singer = singerService.findById(id);
@@ -40,25 +99,12 @@ public class SingerController {
         return "singers/show";
     }
 
-    @RequestMapping(value = "/{id}", params = "form", method = RequestMethod.POST)
-    public String update(@Valid Singer singer, BindingResult bindingResult, Model uiModel,
-                         HttpServletRequest httpServletRequest, RedirectAttributes redirectAttributes,
-                         Locale locale){
-        logger.info("Updating singer");
-        if (bindingResult.hasErrors()){
-            uiModel.addAttribute("message", new Message("error", messageSource.getMessage("singer_save_fail",
-                    new Object[]{}, locale)));
-            uiModel.addAttribute("singer", singer);
-            return "singers/update";
-        }
-        uiModel.asMap().clear();
-        redirectAttributes.addFlashAttribute("message", new Message("success", messageSource.getMessage("singer_save_success",
-                new Object[]{}, locale)));
-        singerService.save(singer);
-        return "redirect:/singers/" + UrlUtil.encodeUrlPathSegment(singer.getId().toString(), httpServletRequest);
+    @RequestMapping(params = "form", method = RequestMethod.GET)
+    public String createForm(Model uiModel){
+        Singer singer = new Singer();
+        uiModel.addAttribute("singer",singer);
+        return "singers/create";
     }
-    @Autowired
-    public void setSingerService(SingerService singerService){
-        this.singerService = singerService;
-    }
+
+
 }
